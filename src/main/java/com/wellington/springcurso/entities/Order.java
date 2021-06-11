59772e602ba9d3ef0834820aa0a85209cfe862f1
Wellington1000 <martinswellingtonsilva@hotmail.com>
2021-06-10 21:20:17 -0300

    Order, Instant, ISO 8601

---
+++ src/main/java/com/wellington/springcurso/entities/Order.java
@@ -0,0 +1,96 @@
+package com.wellington.springcurso.entities;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_order")
+public class Order implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private Instant moment; //'Instant' é semelhante ao Date
+	
+	//Anotações JPA
+	@ManyToOne//'@ManyToOne' transforma 'client' em uma chave estrangeira
+	@JoinColumn(name = "client_id") //'@JoinColumn' Altera o nome da coluna da chave estrangeira para o valor especificado entre parenteses
+	private User client;
+
+	
+	public Order() {}
+	
+	public Order(Long id, Instant moment, User client){
+		super();
+		this.id = id;
+		this.moment = moment;
+		this.client = client;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Instant getMoment() {
+		return moment;
+	}
+
+	public void setMoment(Instant moment) {
+		this.moment = moment;
+	}
+
+	public User getClient() {
+		return client;
+	}
+
+	public void setClient(User client) {
+		this.client = client;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((moment == null) ? 0 : moment.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Order other = (Order) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (moment == null) {
+			if (other.moment != null)
+				return false;
+		} else if (!moment.equals(other.moment))
+			return false;
+		return true;
+	}
+	
+	
+	
+}
